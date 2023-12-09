a897ac9817490da2b603c0565c4564e2107bb3f5
FirstPlayerCoonfurm <119936322+FirstPlayerCoonfurm@users.noreply.github.com>
2023-12-09 16:41:58 +0300

    Add tests and optimize some code

--- SimpleCalcPage.cs
+++ SimpleCalcPage.cs
@@ -1,98 +1,59 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System.Threading;
+
 
 namespace SiteTest
 {
-    internal class SimpleCalcPage
+    public class Tests
     {
         IWebDriver drw;
-        private By buttoninca = By.XPath("//button[@ng-click='inca()']");
-        private By buttondeca = By.XPath("//button[@ng-click='deca()']");
-        private By buttonincb = By.XPath("//button[@ng-click='incb()']");
-        private By buttondecb = By.XPath("//button[@ng-click='decb()']");
-
-        private By fielda = By.XPath("//input[@ng-model='a']");
-        private By fieldb = By.XPath("//input[@ng-model='b']");
-
-        private By operatormns = By.XPath("//select[@class='ng-pristine ng-valid']/option[@value='-']");
-        private By operatorumn = By.XPath("//select[@class='ng-pristine ng-valid']/option[@value='*']");
-        private By operatordel = By.XPath("//select[@class='ng-pristine ng-valid']/option[@value='/']");
-
-
-        public SimpleCalcPage(IWebDriver driwe)
+        [SetUp]
+        public void Setup()
         {
-            drw = driwe;
+            drw = new ChromeDriver();
+            drw.Url = "https://www.globalsqa.com/angularJs-protractor/SimpleCalculator/";
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            drw.Quit();
         }
 
-        public void Clics()
+        [Test]
+        public void Test1()
         {
-            IWebElement button_inca = drw.FindElement(buttoninca);
-            button_inca.Click();
-            Thread.Sleep(1500);
-
-            IWebElement button_deca = drw.FindElement(buttondeca);
-            button_deca.Click();
-            Thread.Sleep(1500);
-
-            IWebElement button_incb = drw.FindElement(buttonincb);
-            button_incb.Click();
-            Thread.Sleep(1500);
-
-            IWebElement button_decb = drw.FindElement(buttondecb);
-            button_decb.Click();
-            Thread.Sleep(1500);
+            var x = new SimpleCalcPage(drw);
+            x.Clics();
         }
-
-        public void AddData()
+        [Test]
+        public void Test2()
         {
-            IWebElement text_a = drw.FindElement(fielda);
-            for (int i = 0; i < 10; i++)
-            {
-                text_a.Clear();
-                text_a.SendKeys($"{i}");
-                Thread.Sleep(1000);
-                text_a.Clear();
-            }
-            text_a.SendKeys("0");
-
-            IWebElement text_b = drw.FindElement(fieldb);
-            for (int i = 0; i < 10; i++)
-            {
-                text_b.Clear();
-                text_b.SendKeys($"{i}");
-                Thread.Sleep(1000);
-                text_b.Clear();
-            }
-            text_b.SendKeys("0");
+            var x = new SimpleCalcPage(drw);
+            x.AddData();
         }
-
-        public void ChangeOperation()
+        [Test]
+        public void Test3()
         {
-            try
-            {
-                Thread.Sleep(1500);
-                IWebElement operator_mn = drw.FindElement(operatormns);
-                operator_mn.Click();
-
-                Thread.Sleep(1500);
-                IWebElement operator_pm = drw.FindElement(operatorumn);
-                operator_pm.Click();
-
-                Thread.Sleep(1500);
-                IWebElement operator_de = drw.FindElement(operatordel);
-                operator_de.Click();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            var x = new SimpleCalcPage(drw);
+            x.ChangeOperation1();
+            x.Clics();
+            x.AddData();
+        }
+        [Test]
+        public void Test4()
+        {
+            var x = new SimpleCalcPage(drw);
+            x.ChangeOperation2();
+            x.Clics();
+            x.AddData();
+        }
+        [Test]
+        public void Test5()
+        {
+            var x = new SimpleCalcPage(drw);
+            x.ChangeOperation3();
+            x.Clics();
+            x.AddData();
         }
     }
 }
